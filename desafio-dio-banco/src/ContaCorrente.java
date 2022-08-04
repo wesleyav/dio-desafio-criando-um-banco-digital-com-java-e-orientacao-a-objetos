d42acc959171a3320fa5ff347add382655c5b949
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-04 10:13:14 -0300

    Encapsulamento

--- desafio-dio-banco/src/ContaCorrente.java
+++ desafio-dio-banco/src/ContaCorrente.java
@@ -1,9 +1,9 @@
 
 public class ContaCorrente {
 
-	public int agencia;
-	public int numero;
-	public double saldo;
+	private int agencia;
+	private int numero;
+	private double saldo;
 
 	public void sacar() {
 
@@ -15,6 +15,18 @@ public class ContaCorrente {
 
 	public void transferir() {
 
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
 	}
 
 }
