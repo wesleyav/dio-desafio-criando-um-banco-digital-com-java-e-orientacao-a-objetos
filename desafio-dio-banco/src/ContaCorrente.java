c3b13b25c4b21cd78f00b42a04d4cad9b325619b
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-04 10:24:24 -0300

    Herança

--- desafio-dio-banco/src/ContaCorrente.java
+++ desafio-dio-banco/src/ContaCorrente.java
@@ -1,32 +1,4 @@
 
-public class ContaCorrente {
-
-	private int agencia;
-	private int numero;
-	private double saldo;
-
-	public void sacar() {
-
-	}
-
-	public void depositar() {
-
-	}
-
-	public void transferir() {
-
-	}
-
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
+public class ContaCorrente extends Conta {
 
 }
