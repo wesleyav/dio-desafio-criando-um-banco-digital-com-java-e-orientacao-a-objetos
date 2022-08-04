500cc7571745bde5bf13631ff356d9d15f61d36d
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-04 11:03:55 -0300

    Saque, depósito, transferência e extrato

--- desafio-dio-banco/src/Conta.java
+++ desafio-dio-banco/src/Conta.java
@@ -7,26 +7,29 @@ public abstract class Conta implements IConta {
 	protected int agencia;
 	protected int numero;
 	protected double saldo;
+	private Cliente cliente;
 
-	public Conta() {
+	public Conta(Cliente cliente) {
 		this.agencia = Conta.AGENCIA_PADRAO;
 		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
 
 	}
 
 	@Override
 	public void sacar(double valor) {
-
+		saldo -= valor;
 	}
 
 	@Override
 	public void depositar(double valor) {
-
+		saldo += valor;
 	}
 
 	@Override
 	public void transferir(double valor, Conta contaDestino) {
-
+		this.sacar(valor);
+		contaDestino.depositar(valor);
 	}
 
 	public int getAgencia() {
@@ -39,6 +42,13 @@ public abstract class Conta implements IConta {
 
 	public double getSaldo() {
 		return saldo;
+	}
+
+	protected void imprimirInfosComuns() {
+		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+		System.out.println(String.format("Agência: %d", this.agencia));
+		System.out.println(String.format("Número: %d", this.numero));
+		System.out.println(String.format("Saldo: %.2f", this.saldo));
 	}
 
 }
---
+++ desafio-dio-banco/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Cliente cliente = new Cliente();
+		cliente.setNome("José");
+
+		Conta cc = new ContaCorrente(cliente);
+		Conta cp = new ContaPoupanca(cliente);
+
+		cc.depositar(100);
+		cc.transferir(100, cp);
+
+		cc.imprimirExtrato();
+		cp.imprimirExtrato();
+	}
+
+}
